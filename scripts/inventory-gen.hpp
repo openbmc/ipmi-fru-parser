8104615b31d7f857902775d915361117c2e26010
Ratan Gupta <ratagupt@in.ibm.com>
2016-12-22 19:00:41 +0530

    VPD Parser for IPMI FRU.
    Script parses the input Yaml file and generates the
    hpp and cpp file which will be used by the ipmi fru parser
    when the write fru message comes from the host.
    
    Change-Id: I3a005b2dd31d7db3137bed9be6df2a0658c48d82
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

---
+++ scripts/inventory-gen.hpp
@@ -0,0 +1,19 @@
+/* !!! WARNING: This is a GENERATED Code..Please do NOT Edit !!! */
+
+#include <iostream>
+#include <map>
+#include <list>
+
+using MetaDataDict = std::map<std::string,std::string>;
+/* Dictionary of Dbus property and the associated metaData.*/
+using PropertiesDict = std::map<std::string,MetaDataDict>;
+
+/* Dictionary of Interface and the Properties. */
+using InterfacePropertiesDict = std::map<std::string,PropertiesDict>;
+
+/* Dictionary of object and the interfaces. */
+using ObjectInterfaceDict = std::map<std::string,InterfacePropertiesDict>;
+
+/* Dictionary of fru and the objects. */
+using FruObjectDict = std::map<std::string,ObjectInterfaceDict>;
+
