44970fa833f686ff1d98cba2f6d8031b7e726dfd
Ratan Gupta <ratagupt@in.ibm.com>
2016-12-30 19:24:10 +0530

    Adding the meta data for the FRU.
    Fetch the fru info from the MRW.
    Get the meta data from the given input file(metaData),
    Create the new YAML file having meta data,which is to be
    consumed by the script which generates the C++ structures.
    
    inventory-gen.yaml is the output file which will be deleted later.
    
    eg:- ./parser.pl -i witherspoon.xml -o inventory-gen.yaml -m fru-types.yaml
    
    Change-Id: I7607854eb4aa0c2b4a4fc46cc0c3137787893b1d
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- scripts/parser.pl
+++ scripts/parser.pl
@@ -1,24 +1,26 @@
+
 #! /usr/bin/perl
 use strict;
- 
-use mrw::Targets; 
+
+use mrw::Targets;
 use mrw::Inventory;
 use Getopt::Long; # For parsing command line arguments
+use YAML::XS 'LoadFile'; # For loading and reading of YAML file
 use Data::Dumper;
-
-my @listofFru; #Array of hash
 
 # Globals
 my $force           = 0;
 my $serverwizFile  = "";
 my $debug           = 0;
 my $outputFile     = "";
+my $metaDataFile   = "";
 my $verbose         = 0;
 
 # Command line argument parsing
 GetOptions(
-"f"   => \$force,             # numeric
+"f"   => \$force,            # numeric
 "i=s" => \$serverwizFile,    # string
+"m=s" => \$metaDataFile,     #string
 "o=s" => \$outputFile,       # string
 "d"   => \$debug,
 "v"   => \$verbose,
@@ -33,10 +35,18 @@ if (($serverwizFile eq "") or ($outputFile eq ""))
 my $targetObj = Targets->new;
 $targetObj->loadXML($serverwizFile);
 
+#open the mrw xml and the metaData file for the fru.
+#Fetch the FRU id,type,object path from the mrw.
+#Get the metadata for that fru from the metadata file.
+#Merge the data into the outputfile.
+
+open(my $fh, '>', $outputFile) or die "Could not open file '$outputFile' $!";
+my $fruTypeConfig = LoadFile($metaDataFile);
+
 my @inventory = Inventory::getInventory($targetObj);
 for my $item (@inventory) {
     my $isFru = 0, my $fruID = 0, my $fruType = "";
-          #And this gets a FRU ID
+    #Interested in FRU_ID and TYPE
     if (!$targetObj->isBadAttribute($item->{TARGET}, "FRU_ID")) {
         $fruID = $targetObj->getAttribute($item->{TARGET}, "FRU_ID");
         $isFru = 1;
@@ -45,77 +55,70 @@ for my $item (@inventory) {
     if (!$targetObj->isBadAttribute($item->{TARGET}, "TYPE")) {
         $fruType = $targetObj->getAttribute($item->{TARGET}, "TYPE");
     }
-    push @listofFru, { FRUID => $fruID,FRUType => $fruType, ObjectPath => $item->{OBMC_NAME} };
-    
 
-=begin
-    if ($isFru) {
-        my $conns = $targetObj->findConnections($item, "LOGICAL_ASSOCIATION");
-        if ($conns ne "") {
-            print "Connections Found \n";
-            # This conn is associated with a FRU
-            for my $conn (@{$conns->{CONN}}) {
-                my $source = $conn->{SOURCE};
-                my $nonFruDeviceName = $source->{OBMC_NAME};
-                #there I can put a check whether this is interested device or not.
-                if (!$targetObj->isBadAttribute($source->{TARGET}, "TYPE")) {
-                    $fruType = $targetObj->getAttribute($source->{TARGET}, "TYPE");
-                    print "FRU TYPE: $fruType\n";
-                }
-            }                
+    printDebug ("FRUID => $fruID,FRUType => $fruType, ObjectPath => $item->{OBMC_NAME}");
+    #Don't write the fru details in to the file
+    #whose type is NA
+    next if ( $fruType eq 'NA');
+    print $fh "- FRUID: ".$fruID;
+    print $fh "\n";
+    print $fh "  Instances:";
+    print $fh "\n";
 
-        }
-    }
-=cut
+    writeToFile($fruType,$item->{OBMC_NAME},$fruTypeConfig,$fh);
 }
+close $fh;
 
-printDebug("\n======================================================================\n");
-printDebug("\nInventory FRU\n");
-my $i = 0;
-
-for $i ( 0 .. $#listofFru ) {
-    printDebug("\n");
-    for my $key ( sort keys %{ $listofFru[$i] }) {
-        my $value = $listofFru[$i]{$key};
-        printDebug( "$key :: $value, ");
-
-    }
-}
-printDebug("\n======================================================================\n");
-
-generateYamlFile();
 #------------------------------------END OF MAIN-----------------------
 
-#Generate the yaml file
-sub generateYamlFile
-{
-    my $fileName = $outputFile;
-    my $groupCopy = '';
-    my $ledCopy = '';
-    open(my $fh, '>', $fileName) or die "Could not open file '$fileName' $!";
+#Get the metdata for the incoming frutype from the loaded config file.
+#Write the FRU data into the output file
 
-    for $i ( 0 .. $#listofFru ) {
-        print $fh "\n";
-        for my $key ( sort keys %{ $listofFru[$i] }) {
-            my $value = $listofFru[$i]{$key};
-            if ( $key eq "FRUID") {
-                print $fh "-FRUID: ".$value;
+sub writeToFile
+{
+    my $fruType = $_[0];#fru type
+    my $instancePath = $_[1];#instance Path
+    my $fruTypeConfig = $_[2];#loaded config file (frutypes)
+    my $fh = $_[3];#file Handle
+
+    #walk over all the fru types and match for the incoming type
+    foreach my $type (@{$fruTypeConfig}) {
+        if  ($type->{FRUType} eq $fruType) {
+            print $fh "    - Instance: ".$instancePath;
+            print $fh "\n";
+            print $fh "      Interfaces:";
+            print $fh "\n";
+            my $interfaces = $type->{Interfaces};
+            #Walk over all the interfaces as it needs to be written
+            foreach my $interface (@{$interfaces}) {
+                print $fh "        - Interface: ".$interface->{Interface};
                 print $fh "\n";
-            } else {
-                print $fh " $key: ".$value;
+                print $fh "          Properties:";
                 print $fh "\n";
+                #walk over all the properties as it needs to be written
+                foreach my $property (@{$interface->{Properties}}) {
+                    #will write property named "Property" first then
+                    #other properties.
+                    print $fh "             - Property: ".$property->{Property};
+                    print $fh "\n";
+                    for my $key (sort keys %{$property}) {
+                        if ($key ne 'Property') {
+                            print $fh "               $key: ".$property->{$key};
+                            print $fh "\n";
+                        }
+                    }
+                }
             }
-            
         }
     }
-    close $fh;
+
 }
 
 # Usage
 sub printUsage
 {
     print "
-    $0 -i [XML filename] -o [Output filename] [OPTIONS]
+    $0 -i [MRW filename] -m [MetaData filename]-o [Output filename] [OPTIONS]
 Options:
     -f = force output file creation even when errors
     -d = debug mode
