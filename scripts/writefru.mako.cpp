a6053f8d4f59ff966416777f1cfe2d22efc50503
Ratan Gupta <ratagupt@in.ibm.com>
2017-02-10 15:59:31 +0530

    Render mako cpp template instead of hpp
    Generate fru-gen.cpp
    
    Change-Id: Id83220e5e2876f4112ba76d698b2062e2a08dd3d
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- scripts/writefru.mako.cpp
+++ scripts/writefru.mako.cpp
@@ -1,30 +1,8 @@
 // !!! WARNING: This is a GENERATED Code..Please do NOT Edit !!!
-#pragma once
-
 #include <iostream>
+#include "frup.hpp"
 
-#include <string>
-#include <list>
-#include <map>
-
-using IPMIFruMetadata = std::string;
-using IPMIFruMetadataValue = std::string;
-using IPMIFruMap = std::map<IPMIFruMetadata,IPMIFruMetadataValue>;
-
-using DbusProperty = std::string;
-using DbusPropertyMap = std::map<DbusProperty,IPMIFruMap>;
-
-using DbusInterface = std::string;
-using DbusInterfaceMap = std::map<DbusInterface,DbusPropertyMap>;
-
-using FruInstancePath = std::string;
-using FruInstanceMap = std::map<FruInstancePath,DbusInterfaceMap>;
-
-using FruId = uint32_t;
-using FruMap = std::map<FruId,FruInstanceMap>;
-
-
-const FruMap frus = {
+extern const FruMap frus = {
 % for key in fruDict.iterkeys():
    {${key},{
 <%
