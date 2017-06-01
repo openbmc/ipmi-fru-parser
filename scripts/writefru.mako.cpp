5098b03916fd7a90605b6e3774ee536ef9108a94
Patrick Williams <patrick@stwcx.xyz>
2017-06-01 15:40:10 -0500

    fru_gen: fixes for python3
    The iter{keys,items} functions have become just 'keys' and 'items'
    in python3.  Python2 also has keys and items, which is slightly
    less efficient for iteration but none of these maps are large enough
    to be of concern.
    
    Change-Id: If6e8f36ad39074e0a6c022fb09e3a43b0928af19
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- scripts/writefru.mako.cpp
+++ scripts/writefru.mako.cpp
@@ -3,18 +3,18 @@
 #include "frup.hpp"
 
 extern const FruMap frus = {
-% for key in fruDict.iterkeys():
+% for key in fruDict.keys():
    {${key},{
 <%
     fru = fruDict[key]
 %>
-    % for object,interfaces in fru.iteritems():
+    % for object,interfaces in fru.items():
          {"${object}",{
-         % for interface,properties in interfaces.iteritems():
+         % for interface,properties in interfaces.items():
              {"${interface}",{
-            % for dbus_property,property_value in properties.iteritems():
+            % for dbus_property,property_value in properties.items():
                  {"${dbus_property}",{
-                % for name,value in property_value.iteritems():
+                % for name,value in property_value.items():
                      {"${name}","${value}"},
                 % endfor
                  }},
