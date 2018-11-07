a8dd25958c99dc7d6cf4ac81cf3a81e93dfb1c8a
Alexander Soldatov <a.soldatov@yadro.com>
2018-11-07 19:31:13 +0300

    Add support for FRU multi-record areas
    Add parsing of multi-record areas as per IPMI Platform Management
    FRU Information Storage Definition v1.0 revision 1.2.
    
    So far only support the following record types (required to properly
    parse FRUs of CRPS power supply units):
     - Power Supply information (section 18.1)
     - DC Output (section 18.2)
    
    Change-Id: I7087e4be21fbe0b420fb53632712bc8d81545ffa
    Signed-off-by: Alexander Soldatov <a.soldatov@yadro.com>
    Signed-off-by: Alexander Amelkin <a.amelkin@yadro.com>
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

--- frup.cpp
+++ frup.cpp
@@ -692,7 +692,7 @@ void _append_to_dict(uint8_t vpd_key_id, uint8_t* vpd_key_val,
 }
 
 int parse_fru_area(const uint8_t area, const void* msgbuf, const size_t len,
-                   IPMIFruInfo& info)
+                   IPMIFruInfo& info, IPMIMultiInfo& multiInfo)
 {
     int rv = -1;
     int i = 0;
@@ -804,8 +804,12 @@ int parse_fru_area(const uint8_t area, const void* msgbuf, const size_t len,
                 _append_to_dict(i, vpd_info[i].type_length_field, info);
             }
             break;
+        case IPMI_FRU_AREA_MULTI_RECORD:
+            parseMultirecord(static_cast<const uint8_t*>(msgbuf), len,
+                             &multiInfo);
+            break;
         default:
-            /* TODO: Parse Multi Rec / Internal use area */
+            /* TODO: Internal use area */
             break;
     }
 
