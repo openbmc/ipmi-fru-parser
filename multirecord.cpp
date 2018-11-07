179503d9d710969fe58af704b0125a01ec8be9a9
Alexander Filippov <a.filippov@yadro.com>
2018-11-07 19:31:13 +0300

    Add support for FRU multi-record areas
    Add parsing of multi-record areas as per IPMI Platform Management
    FRU Information Storage Definition v1.0 revision 1.2.
    
    So far only support the following record types (required to properly
    parse FRUs of CRPS power supply units):
     - Power Supply information (section 18.1)
     - DC Output (section 18.2)
    
    Change-Id: I7087e4be21fbe0b420fb53632712bc8d81545ffa
    Signed-off-by: Alexander Soldatov <a.soldatov@yadro.com>
    Signed-off-by: Alexander Amelkin <a.amelkin@yadro.com>
    Signed-off-by: Alexander Filippov <a.filippov@yadro.com>

---
+++ multirecord.cpp
@@ -0,0 +1,224 @@
+/**
+ * @brief IPMI FRU MultiRecord Area parser
+ *
+ * This file is part of phosphor-ipmi-fru project.
+ *
+ * Copyright (c) 2018 YADRO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author: Alexander Soldatov <a.soldatov@yadro.com>
+ */
+
+#include <multirecord.hpp>
+#include <phosphor-logging/log.hpp>
+#include <sstream>
+#include <writefrudata.hpp>
+
+using namespace phosphor::logging;
+
+static uint8_t recordSizes[TOTAL_MR_TYPES] = {
+    [POWER_SUPPLY_INFO] = sizeof(PowerSupplyInfo),
+    [DC_OUTPUT] = sizeof(DcOutputInfo)};
+
+#ifdef IPMI_FRU_DEBUG
+std::string joinHex(const std::string& prefix, const uint8_t* data,
+                    const size_t size)
+{
+    std::stringstream stream;
+    stream << prefix;
+    for (size_t i = 0; i < size; ++i)
+        stream << " " << std::hex << static_cast<int>(data[i]);
+    return stream.str();
+}
+#endif
+
+const uint8_t* parseRecord(const uint8_t* recordData, TypedRecord* record)
+{
+    uint8_t headerSize = sizeof(MultirecordHeader);
+#ifdef IPMI_FRU_DEBUG
+    log<level::DEBUG>(
+        joinHex("Record header:", recordData, headerSize).c_str());
+#endif
+    // verify header checksum
+    uint8_t checksum = calculateCRC(recordData, headerSize);
+
+    if (checksum)
+    {
+        log<level::ERR>("Invalid header checksum");
+        return NULL;
+    }
+
+    // parse header
+    MultirecordHeader header;
+    memcpy(&header, recordData, headerSize);
+
+    // set type
+    record->type = static_cast<ImpiMultirecordType>(header.recordTypeId);
+
+    if (record->type > MAX_PARSE_TYPE)
+    {
+        log<level::ERR>("Unsupported record type");
+        return NULL;
+    }
+
+    recordData += headerSize;
+
+    // verify record length
+    if (header.recordLength != recordSizes[header.recordTypeId])
+    {
+        log<level::ERR>("Invalid record length");
+        return NULL;
+    }
+#ifdef IPMI_FRU_DEBUG
+    log<level::DEBUG>(
+        joinHex("Record data:", recordData, header.recordLength).c_str());
+#endif
+
+    // verify checksum
+    checksum =
+        calculateCRC(recordData, header.recordLength) - header.recordChecksum;
+
+    if (checksum)
+    {
+        log<level::ERR>("Invalid record checksum");
+        return NULL;
+    }
+
+    // copy data
+    memcpy(&(record->data), recordData, header.recordLength);
+
+    if (header.endOfList())
+    {
+        recordData = NULL;
+    }
+    else
+    {
+        recordData += header.recordLength;
+    }
+    return recordData;
+}
+
+void parseMultirecord(const uint8_t* fruData, const size_t dataLen,
+                      IPMIMultiInfo* info)
+{
+    const uint8_t* recordData = fruData;
+    TypedRecord record;
+    while (recordData && static_cast<size_t>(recordData - fruData) <= dataLen)
+    {
+        recordData = parseRecord(recordData, &record);
+        info->push_back(record);
+#ifdef IPMI_FRU_PARSER_DEBUG
+        switch (record.type)
+        {
+            case POWER_SUPPLY_INFO:
+                log<level::DEBUG>(
+                    "Record PowerSupplyInfo",
+                    entry("CAPACITY=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.overallCapacity)),
+                    entry("PEAK_VA=%d",
+                          static_cast<int>(record.data.powerSupplyInfo.peakVa)),
+                    entry("INRUSH_CURRENT=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.inrushCurrent)),
+                    entry("INRUSH_INTERVAL=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.inrushInterval)),
+                    entry("LOW_IN_VOLTAGE1=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.lowInVoltage1)),
+                    entry("HIGH_IN_VOLTAGE1=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.highInVoltage1)),
+                    entry("LOW_IN_VOLTAGE2=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.lowInVoltage2)),
+                    entry("HIGH_IN_VOLTAGE2=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.highInVoltage2)),
+                    entry("LOW_IN_FREQUENCY=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.lowInFrequency)),
+                    entry("HIGH_IN_FREQUENCY=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.highInFrequency)),
+                    entry("IN_DROPOUT_TOLERANCE=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.inDropoutTolerance)),
+                    entry("PIN_POLARITY=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.pinPolarity())),
+                    entry("HOT_SWAP=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.hotSwap())),
+                    entry("AUTOSWITCH=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.autoswitch())),
+                    entry("POWER_CORRECTION=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.powerCorrection())),
+                    entry("PREDICTIVE_FAIL=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.predictiveFail())),
+                    entry("HOLD_UP_TIME=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.holdUpTime())),
+                    entry("VOLTAGE1=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.voltage1())),
+                    entry("VOLTAGE2=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.voltage2())),
+                    entry("TOTAL_WATTAGE=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.totalWattage)),
+                    entry("TACHOMETER_LOW=%d",
+                          static_cast<int>(
+                              record.data.powerSupplyInfo.tachometerLow)));
+                break;
+            case DC_OUTPUT:
+                log<level::DEBUG>(
+                    "Record DcOutputInfo",
+                    entry("NOMINAL_VOLTAGE=%d",
+                          static_cast<int>(
+                              record.data.dcOutputInfo.nominalVoltage)),
+                    entry("MAX_NEGATIVE_VOLTAGE=%d",
+                          static_cast<int>(
+                              record.data.dcOutputInfo.maxNegativeVoltage)),
+                    entry("MAX_POSITIVE_VOLTAGE=%d",
+                          static_cast<int>(
+                              record.data.dcOutputInfo.maxPositiveVoltage)),
+                    entry("RIPPLE_AND_NOISE=%d",
+                          static_cast<int>(
+                              record.data.dcOutputInfo.rippleAndNoise)),
+                    entry("MIN_CURRENT_DRAW=%d",
+                          static_cast<int>(
+                              record.data.dcOutputInfo.minCurrentDraw)),
+                    entry("MAX_CURRENT_DRAW=%d",
+                          static_cast<int>(
+                              record.data.dcOutputInfo.maxCurrentDraw)),
+                    entry(
+                        "OUT_NUMBER=%d",
+                        static_cast<int>(record.data.dcOutputInfo.outNumber())),
+                    entry(
+                        "STANDBY=%d",
+                        static_cast<int>(record.data.dcOutputInfo.standby())));
+                break;
+            default:
+                log<level::DEBUG>("unsupported record");
+                break;
+        }
+#endif
+    }
+}
--- writefrudata.cpp
+++ writefrudata.cpp
@@ -1,9 +1,3 @@
-#include "writefrudata.hpp"
-
-#include "fru_area.hpp"
-#include "frup.hpp"
-#include "types.hpp"
-
 #include <host-ipmid/ipmid-api.h>
 #include <unistd.h>
 
@@ -11,6 +5,8 @@
 #include <cstdio>
 #include <cstring>
 #include <exception>
+#include <fru_area.hpp>
+#include <frup.hpp>
 #include <fstream>
 #include <iostream>
 #include <map>
@@ -18,7 +14,9 @@
 #include <phosphor-logging/log.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sstream>
+#include <types.hpp>
 #include <vector>
+#include <writefrudata.hpp>
 
 using namespace ipmi::vpd;
 using namespace phosphor::logging;
@@ -30,7 +28,6 @@ using FruAreaVector = std::vector<std::unique_ptr<IPMIFruArea>>;
 
 namespace
 {
-
 //------------------------------------------------------------
 // Cleanup routine
 // Must always be called as last reference to fruFilePointer.
@@ -142,6 +139,63 @@ auto getService(sdbusplus::bus::bus& bus, const std::string& intf,
     return mapperResponse.begin()->first;
 }
 
+// MultiRecord area record name and num matching.
+// It is used by ipmiUpdateMultirecord function.
+std::map<std::string, ImpiMultirecordType> multirecordMap = {
+    {"PowerSupply", POWER_SUPPLY_INFO}, {"DcOutput", DC_OUTPUT}};
+
+//------------------------------------------------------------------------
+// Adding property values for Power Supply Information record
+//------------------------------------------------------------------------
+void ipmiUpdatePsuInfo(const PowerSupplyInfo& data, PropertyMap& props)
+{
+    props.emplace("OverallCapacity",
+                  static_cast<int64_t>(data.overallCapacity));
+    props.emplace("PeakVA", static_cast<int64_t>(data.peakVa));
+    props.emplace("InrushCurrent", static_cast<int64_t>(data.inrushCurrent));
+    props.emplace("InrushInterval", static_cast<int64_t>(data.inrushInterval));
+    props.emplace("LowInputVoltage1", static_cast<int64_t>(data.lowInVoltage1));
+    props.emplace("HighInputVoltage1",
+                  static_cast<int64_t>(data.highInVoltage1));
+    props.emplace("LowInputVoltage2", static_cast<int64_t>(data.lowInVoltage2));
+    props.emplace("HighInputVoltage2",
+                  static_cast<int64_t>(data.highInVoltage2));
+    props.emplace("LowInputFrequency",
+                  static_cast<int64_t>(data.lowInFrequency));
+    props.emplace("HighInputFrequency",
+                  static_cast<int64_t>(data.highInFrequency));
+    props.emplace("InDropoutTolerance",
+                  static_cast<int64_t>(data.inDropoutTolerance));
+    props.emplace("HoldUpTime", static_cast<int64_t>(data.holdUpTime()));
+    props.emplace("PeakCapacity", static_cast<int64_t>(data.peakCapacity()));
+    props.emplace("Voltage1", static_cast<int64_t>(data.voltage1()));
+    props.emplace("Voltage2", static_cast<int64_t>(data.voltage2()));
+    props.emplace("TotalWattage", static_cast<int64_t>(data.totalWattage));
+    props.emplace("TachometerLow", static_cast<int64_t>(data.tachometerLow));
+    props.emplace("HotSwap", data.hotSwap());
+    props.emplace("PinPolarity", data.pinPolarity());
+    props.emplace("Autoswitch", data.autoswitch());
+    props.emplace("PowerFactor", data.powerCorrection());
+    props.emplace("PredictiveFail", data.predictiveFail());
+}
+
+//------------------------------------------------------------------------
+// Adding property values for DC Output record
+//------------------------------------------------------------------------
+void ipmiUpdateDcLoad(const DcOutputInfo& data, PropertyMap& props)
+{
+    props.emplace("OutputNumber", static_cast<int64_t>(data.outNumber()));
+    props.emplace("NominalVoltage", static_cast<int64_t>(data.nominalVoltage));
+    props.emplace("MaxNegativeVoltage",
+                  static_cast<int64_t>(data.maxNegativeVoltage));
+    props.emplace("MaxPositiveVoltage",
+                  static_cast<int64_t>(data.maxPositiveVoltage));
+    props.emplace("RippleAndNoise", static_cast<int64_t>(data.rippleAndNoise));
+    props.emplace("MinCurrentDraw", static_cast<int64_t>(data.minCurrentDraw));
+    props.emplace("MaxCurrentDraw", static_cast<int64_t>(data.maxCurrentDraw));
+    props.emplace("Standby", data.standby());
+}
+
 // Takes FRU data, invokes Parser for each fru record area and updates
 // Inventory
 //------------------------------------------------------------------------
@@ -151,6 +205,7 @@ int updateInventory(FruAreaVector& areaVector, sdbusplus::bus::bus& bus)
     int rc = 0;
     uint8_t fruid = 0;
     IPMIFruInfo fruData;
+    IPMIMultiInfo multiData;
 
     // For each FRU area, extract the needed data , get it parsed and update
     // the Inventory.
@@ -160,7 +215,7 @@ int updateInventory(FruAreaVector& areaVector, sdbusplus::bus::bus& bus)
         // Fill the container with information
         rc = parse_fru_area(fruArea->getType(),
                             static_cast<const void*>(fruArea->getData()),
-                            fruArea->getLength(), fruData);
+                            fruArea->getLength(), fruData, multiData);
         if (rc < 0)
         {
             log<level::ERR>("Error parsing FRU records");
@@ -192,8 +247,7 @@ int updateInventory(FruAreaVector& areaVector, sdbusplus::bus::bus& bus)
     auto iter = frus.find(fruid);
     if (iter == frus.end())
     {
-        log<level::ERR>("Unable to get the fru info",
-                        entry("FRU=%d", static_cast<int>(fruid)));
+        log<level::ERR>("Unable to get the fru info", entry("FRU=%d", fruid));
         return -1;
     }
 
@@ -201,7 +255,7 @@ int updateInventory(FruAreaVector& areaVector, sdbusplus::bus::bus& bus)
     if (instanceList.size() <= 0)
     {
         log<level::DEBUG>("Object list empty for this FRU",
-                          entry("FRU=%d", static_cast<int>(fruid)));
+                          entry("FRU=%d", fruid));
     }
 
     ObjectMap objects;
@@ -209,6 +263,7 @@ int updateInventory(FruAreaVector& areaVector, sdbusplus::bus::bus& bus)
     {
         InterfaceMap interfaces;
         const auto& extrasIter = extras.find(instance.path);
+        bool skipObject = false;
 
         for (const auto& interfaceList : instance.interfaces)
         {
@@ -217,6 +272,49 @@ int updateInventory(FruAreaVector& areaVector, sdbusplus::bus::bus& bus)
             {
                 std::string value;
                 decltype(auto) pdata = properties.second;
+
+                // if object refers to multirecord data, skip it
+                if (pdata.section == "MultiRecord" &&
+                    properties.first == "Data")
+                {
+                    auto mapIt = multirecordMap.find(pdata.property);
+                    if (mapIt == multirecordMap.end())
+                        continue;
+
+                    int i = 0;
+                    for (auto it = multiData.begin(); it != multiData.end();
+                         ++it)
+                    {
+                        if (it->type == mapIt->second)
+                        {
+                            PropertyMap props;
+                            InterfaceMap interfaces;
+                            switch (mapIt->second)
+                            {
+                                case POWER_SUPPLY_INFO:
+                                    ipmiUpdatePsuInfo(it->data.powerSupplyInfo,
+                                                      props);
+                                    break;
+                                case DC_OUTPUT:
+                                    ipmiUpdateDcLoad(it->data.dcOutputInfo,
+                                                     props);
+                                    break;
+                                default:
+                                    continue;
+                                    break;
+                            }
+                            interfaces.emplace(std::move(interfaceList.first),
+                                               std::move(props));
+                            sdbusplus::message::object_path path =
+                                instance.path + std::to_string(i);
+                            objects.emplace(path, interfaces);
+                            ++i;
+                        }
+                    }
+
+                    skipObject = true;
+                    continue;
+                }
 
                 if (!pdata.section.empty() && !pdata.property.empty())
                 {
@@ -255,7 +353,11 @@ int updateInventory(FruAreaVector& areaVector, sdbusplus::bus::bus& bus)
                 }
             }
         }
-        objects.emplace(objectPath, interfaces);
+
+        if (!skipObject)
+        {
+            objects.emplace(objectPath, interfaces);
+        }
     }
 
     auto pimMsg = bus.new_method_call(service.c_str(), path.c_str(),
@@ -427,6 +529,30 @@ int ipmiPopulateFruAreas(uint8_t* fruData, const size_t dataLen,
 
             // Size of this area will be the 2nd byte in the fru area header.
             size_t areaLen = areaHeader[1] * IPMI_EIGHT_BYTES;
+
+            // We skip checking for some entries because:
+            //  * The `Internal Use Area` has an unknown format
+            //    and we can't validate it.
+            //  * The `MultiRecord Area` has different format and
+            //    it will checked later.
+            bool isCheckable = (fruEntry != IPMI_FRU_INTERNAL_OFFSET &&
+                                fruEntry != IPMI_FRU_MULTI_OFFSET);
+
+            if (!isCheckable)
+            {
+                areaLen = dataLen - areaOffset;
+                for (uint8_t i = fruEntry + 1;
+                     i < (sizeof(struct common_header) - 2); ++i)
+                {
+                    if (fruData[i] > fruData[fruEntry])
+                    {
+                        areaLen =
+                            (fruData[i] - fruData[fruEntry]) * IPMI_EIGHT_BYTES;
+                        break;
+                    }
+                }
+            }
+
             uint8_t areaData[areaLen] = {0};
 
             log<level::DEBUG>("Fru Data", entry("SIZE=%d", dataLen),
@@ -445,18 +571,21 @@ int ipmiPopulateFruAreas(uint8_t* fruData, const size_t dataLen,
             // Save off the data.
             std::memcpy(areaData, &((uint8_t*)fruData)[areaOffset], areaLen);
 
-            // Validate the crc
-            rc = verifyFruData(areaData, areaLen);
-            if (rc < 0)
+            if (isCheckable)
             {
-                log<level::ERR>("Err validating fru area",
-                                entry("OFFSET=%d", areaOffset));
-                return rc;
-            }
-            else
-            {
-                log<level::DEBUG>("Successfully verified area checksum.",
-                                  entry("OFFSET=%d", areaOffset));
+                // Validate the crc
+                rc = verifyFruData(areaData, areaLen);
+                if (rc < 0)
+                {
+                    log<level::ERR>("Err validating fru area",
+                                    entry("OFFSET=%d", areaOffset));
+                    continue;
+                }
+                else
+                {
+                    log<level::DEBUG>("Successfully verified area checksum.",
+                                      entry("OFFSET=%d", areaOffset));
+                }
             }
 
             // We already have a vector that is passed to us containing all
@@ -520,8 +649,8 @@ int validateFRUArea(const uint8_t fruid, const char* fruFilename,
     size_t bytesRead = 0;
     int rc = -1;
 
-    // Vector that holds individual IPMI FRU AREAs. Although MULTI and INTERNAL
-    // are not used, keeping it here for completeness.
+    // Vector that holds individual IPMI FRU AREAs. Although INTERNAL
+    // is not used, keeping it here for completeness.
     FruAreaVector fruAreaVec;
 
     for (uint8_t fruEntry = IPMI_FRU_INTERNAL_OFFSET;
