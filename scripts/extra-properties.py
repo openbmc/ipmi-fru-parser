f2c61d125b77d2cd885c3da11a853ef305ccb37f
Patrick Williams <patrick@stwcx.xyz>
2022-12-08 06:18:06 -0600

    black: re-format
    black and isort are enabled in the openbmc-build-scripts on Python files
    to have a consistent formatting.  Re-run the formatter on the whole
    repository.
    
    Change-Id: I5e068068ef3faa2f2c77bc1726b134eb6268dcfb
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- scripts/extra-properties.py
+++ scripts/extra-properties.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
+
 import yaml
 from mako.template import Template
-import argparse
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="IPMI FRU VPD parser and code generator")
+        description="IPMI FRU VPD parser and code generator"
+    )
 
     parser.add_argument(
-        '-e', '--extra_props_yaml',
-        dest='extra_props_yaml',
-        default='extra-properties-example.yaml',
-        help='input extra properties yaml file to parse')
+        "-e",
+        "--extra_props_yaml",
+        dest="extra_props_yaml",
+        default="extra-properties-example.yaml",
+        help="input extra properties yaml file to parse",
+    )
     args = parser.parse_args()
 
-    with open(os.path.join(script_dir, args.extra_props_yaml), 'r') as fd:
+    with open(os.path.join(script_dir, args.extra_props_yaml), "r") as fd:
         yamlDict = yaml.safe_load(fd)
 
         # Render the mako template
-        template = os.path.join(script_dir, 'extra-properties.mako.cpp')
+        template = os.path.join(script_dir, "extra-properties.mako.cpp")
         t = Template(filename=template)
-        with open('extra-properties-gen.cpp', 'w') as fd:
-            fd.write(
-                t.render(
-                    dict=yamlDict))
+        with open("extra-properties-gen.cpp", "w") as fd:
+            fd.write(t.render(dict=yamlDict))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
     main()
